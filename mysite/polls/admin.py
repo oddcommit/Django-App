d0c57b0aea3379f56f9e55e4758f7abba233cac6
flexcommits <andersensven.star@gmail.com>
2023-07-08 23:13:22 +0800

    feat: add the polls to the admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"],
+        "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
