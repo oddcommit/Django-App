29721b16b9c666dc0f83c231678974899293fadc
flexcommits <andersensven.star@gmail.com>
2023-07-08 23:04:19 +0800

    feat: implement of fieldsets

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
